f24a142fd499ff1f3d91437bcf80e0395d393999
Tianhua Han <tiahua_han@yahoo.com>
2026-03-26 12:14:27 +0100

    update

--- parts/Npt_f.py
+++ parts/Npt_f.py
@@ -9,19 +9,17 @@ from cadcoder.objtools import update_obj_prop_jsonDict
 from cadcoder.subelementtools import update_objs_seName, update_doc_seName, get_seName_by_posName
 
 class npt_f(baseClass):
-    def __init__(self, instanceName, doc, objPrefix="", useLabel=True, importer=None, femaleOD_wall_spec='0.08 in', female_height_spec='0.6 in', holeDiaExpansion_spec='0.03 in', horizontalScale=1.1982, nominalID='`1/16', verticalScale=1.261,  ):
-        self.femaleOD_wall_spec = femaleOD_wall_spec
-        self.female_height_spec = female_height_spec
-        self.holeDiaExpansion_spec = holeDiaExpansion_spec
-        self.horizontalScale = horizontalScale
+    def __init__(self, instanceName, doc, objPrefix="", useLabel=True, importer=None, diaExpansion='0.03 in', femaleOD_wall='0.08 in', female_height='0.6 in', nominalID='`2',  ):
+        self.diaExpansion = diaExpansion
+        self.femaleOD_wall = femaleOD_wall
+        self.female_height = female_height
         self.nominalID = nominalID
-        self.verticalScale = verticalScale
         
         super().__init__(instanceName, doc, objPrefix=objPrefix, useLabel=useLabel, importer=importer)
         
         # import classes and create instances for directly imported objects
         from parts.npt_m import npt_m
-        npt_m_instance = npt_m('npt_m_instance', doc, objPrefix=self.objPrefix + 'npt_m_', useLabel=True, importer=self, holeDiaExpansion_spec='0.03 in', horizontalScale=1.1982, male_height_spec='0.6 in', nominalOD='`1/16', verticalScale=1.261, )
+        npt_m_instance = npt_m('npt_m_instance', doc, objPrefix=self.objPrefix + 'npt_m_', useLabel=True, importer=self, diaExpansion='0.03 in', male_height='0.6 in', nominalOD='`2', )
         self.npt_m_instance = npt_m_instance # expose as instance variable
         self.update_imports(npt_m_instance) # update import info for the instance
         
@@ -58,50 +56,29 @@ class npt_f(baseClass):
         self.callsheet = callsheet
         self.post_new_obj(callsheet)
         callsheet.set('A1', 'variableName')
-        callsheet.set('A10', 'holeDiaExpansion_spec')
         callsheet.set('A2', 'nominalID')
-        callsheet.set('A3', 'horizontalScale')
-        callsheet.set('A4', 'verticalScale')
-        callsheet.set('A5', 'femaleOD_wall_spec')
-        callsheet.set('A6', 'femaleOD_spec')
-        callsheet.set('A7', 'femaleOD')
-        callsheet.set('A8', 'female_height_spec')
-        callsheet.set('A9', 'female_height')
+        callsheet.set('A3', 'femaleOD_wall')
+        callsheet.set('A4', 'femaleOD')
+        callsheet.set('A5', 'female_height')
+        callsheet.set('A6', 'diaExpansion')
         callsheet.set('B1', 'value')
-        callsheet.set('B10', '=0.03 in')
-        callsheet.setAlias('B10', 'holeDiaExpansion_spec')
-        callsheet.set('B2', '`1/16')
+        callsheet.set('B2', '`2')
         callsheet.setAlias('B2', 'nominalID')
-        callsheet.set('B3', '1.1982')
-        callsheet.setAlias('B3', 'horizontalScale')
-        callsheet.set('B4', '1.261')
-        callsheet.setAlias('B4', 'verticalScale')
-        callsheet.set('B5', '=0.08 in')
-        callsheet.setAlias('B5', 'femaleOD_wall_spec')
-        callsheet.set('B6', '=0.5025000000000001 in')
-        callsheet.setAlias('B6', 'femaleOD_spec')
-        callsheet.set('B7', '=0.6020955 in')
-        callsheet.setAlias('B7', 'femaleOD')
-        callsheet.set('B8', '=0.6 in')
-        callsheet.setAlias('B8', 'female_height_spec')
-        callsheet.set('B9', '=0.7565999999999998 in')
-        callsheet.setAlias('B9', 'female_height')
+        callsheet.set('B3', '=0.08 in')
+        callsheet.setAlias('B3', 'femaleOD_wall')
+        callsheet.set('B4', '=2.565 in')
+        callsheet.setAlias('B4', 'femaleOD')
+        callsheet.set('B5', '=0.6 in')
+        callsheet.setAlias('B5', 'female_height')
+        callsheet.set('B6', '=0.03 in')
+        callsheet.setAlias('B6', 'diaExpansion')
         callsheet.set('C1', 'isCallParam')
-        callsheet.set('C10', 'Y')
         callsheet.set('C2', 'Y')
         callsheet.set('C3', 'Y')
-        callsheet.set('C4', 'Y')
+        callsheet.set('C4', 'N')
         callsheet.set('C5', 'Y')
-        callsheet.set('C6', 'N')
-        callsheet.set('C7', 'N')
-        callsheet.set('C8', 'Y')
-        callsheet.set('C9', 'N')
+        callsheet.set('C6', 'Y')
         callsheet.set('D1', 'comment')
-        callsheet.set('D3', 'BASF 17-4 xy scale 1.1982')
-        callsheet.set('D4', 'BASF 17-4 z scale 1.2610')
-        callsheet.set('D6', '`femaleOD_wall * 2 + <<npt_m_spec>>.RealOD + holeDiaExpansion_spec')
-        callsheet.set('D7', 'scaled')
-        callsheet.set('D9', 'scaled')
         callsheet.recompute()  # recompute after adding object
         
         sketch = doc.addObject('Sketcher::SketchObject', self.addPrefix('sketch') )
@@ -109,9 +86,9 @@ class npt_f(baseClass):
         self.sketch = sketch
         self.post_new_obj(sketch)
         self.container_append_object(body, sketch)
-        geo0 = sketch.addGeometry(Part.Circle(Vector(0.0000, 0.0000, 0.0000), Vector (0.0, 0.0, 1.0), 7.6466))
+        geo0 = sketch.addGeometry(Part.Circle(Vector(0.0000, 0.0000, 0.0000), Vector (0.0, 0.0, 1.0), 32.5755))
         sketch.addConstraint(Sketcher.Constraint('Coincident', geo0, 3, -1, 1))
-        sketch.addConstraint(Sketcher.Constraint('Diameter', geo0, 15.2932))
+        sketch.addConstraint(Sketcher.Constraint('Diameter', geo0, 65.1510))
         sketch.AttacherEngine = 'Engine Plane'
         sketch.AttachmentSupport = (body_XY_Plane, (''))
         body_XY_Plane.Visibility = False  # hide base object
@@ -125,7 +102,7 @@ class npt_f(baseClass):
         self.pad = pad
         self.post_new_obj(pad)
         self.container_append_object(body, pad)
-        pad.Length = 19.217639999999996
+        pad.Length = 15.239999999999998
         pad.Profile = (sketch, [''])
         pad.ReferenceAxis = (sketch, ['N_Axis'])
         pad.Visibility = False
@@ -146,15 +123,11 @@ class npt_f(baseClass):
         # add delayed static property values
         
         # add expressions to object properties based on expression dependencies
-        npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('male_height_spec'), f"=<<{self.addPrefix('callsheet')}>>.female_height_spec")
         npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('nominalOD'), f"=<<{self.addPrefix('callsheet')}>>.nominalID")
-        npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('horizontalScale'), f"=<<{self.addPrefix('callsheet')}>>.horizontalScale")
-        npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('verticalScale'), f"=<<{self.addPrefix('callsheet')}>>.verticalScale")
-        npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('holeDiaExpansion_spec'), f"=<<{self.addPrefix('callsheet')}>>.holeDiaExpansion_spec")
-        callsheet.set(callsheet.getCellFromAlias("female_height"), f"=female_height_spec * verticalScale")
+        npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('diaExpansion'), f"=<<{self.addPrefix('callsheet')}>>.diaExpansion")
+        npt_m_instance.callsheet.set(npt_m_instance.callsheet.getCellFromAlias('male_height'), f"=<<{self.addPrefix('callsheet')}>>.female_height")
         pad.setExpression("Length", f"<<{self.addPrefix('callsheet')}>>.female_height")
-        callsheet.set(callsheet.getCellFromAlias("femaleOD_spec"), f"=femaleOD_wall_spec * 2 + <<{self.addPrefix('npt_m_spec')}>>.RealOD + holeDiaExpansion_spec")
-        callsheet.set(callsheet.getCellFromAlias("femaleOD"), f"=femaleOD_spec * horizontalScale")
+        callsheet.set(callsheet.getCellFromAlias("femaleOD"), f"=femaleOD_wall * 2 + <<{self.addPrefix('npt_m_spec')}>>.RealOD + diaExpansion")
         sketch.setExpression("Constraints[1]", f"<<{self.addPrefix('callsheet')}>>.femaleOD")
         
         # add trigger objects' expressions
@@ -164,12 +137,10 @@ class npt_f(baseClass):
         # now we have rebuilt the original npt_f doc. Now we apply dynmic call parameters
         print("there can be temporary errors when we applying dynamic call parameters that change original npt_f's shape.")
         print("ignore temporary errors, if any, below.")
-        callsheet.set(callsheet.getCellFromAlias('holeDiaExpansion_spec'), f'={self.holeDiaExpansion_spec}')
         callsheet.set(callsheet.getCellFromAlias('nominalID'), f'{self.nominalID}')
-        callsheet.set(callsheet.getCellFromAlias('horizontalScale'), f'{self.horizontalScale}')
-        callsheet.set(callsheet.getCellFromAlias('verticalScale'), f'{self.verticalScale}')
-        callsheet.set(callsheet.getCellFromAlias('femaleOD_wall_spec'), f'={self.femaleOD_wall_spec}')
-        callsheet.set(callsheet.getCellFromAlias('female_height_spec'), f'={self.female_height_spec}')
+        callsheet.set(callsheet.getCellFromAlias('femaleOD_wall'), f'={self.femaleOD_wall}')
+        callsheet.set(callsheet.getCellFromAlias('female_height'), f'={self.female_height}')
+        callsheet.set(callsheet.getCellFromAlias('diaExpansion'), f'={self.diaExpansion}')
         doc.recompute()
         update_doc_seName(doc, refreshCache=True) # call params may change shape, so we update face/edge names.
         print("ignore temporary errors, if any, above.")
@@ -183,7 +154,7 @@ def main():
     doc = recreate_tmp_doc()
     
     # create instance of npt_f
-    myInstance = npt_f("myInstance", doc, objPrefix="", useLabel=True, importer=None, femaleOD_wall_spec='0.08 in', female_height_spec='0.6 in', holeDiaExpansion_spec='0.03 in', horizontalScale=1.1982, nominalID='`1/16', verticalScale=1.261, )
+    myInstance = npt_f("myInstance", doc, objPrefix="", useLabel=True, importer=None, diaExpansion='0.03 in', femaleOD_wall='0.08 in', female_height='0.6 in', nominalID='`2', )
     
     # main_part2
     from pprint import pformat
--- parts/Npt_fxf.py
+++ parts/Npt_fxf.py
@@ -9,28 +9,26 @@ from cadcoder.objtools import update_obj_prop_jsonDict
 from cadcoder.subelementtools import update_objs_seName, update_doc_seName, get_seName_by_posName
 
 class npt_fxf(baseClass):
-    def __init__(self, instanceName, doc, objPrefix="", useLabel=True, importer=None, b_npt_f_height_spec='0.5 in', b_npt_f_nominalID='`2', b_npt_f_wall_spec='0.08 in', base_plate_thick_spec='0.13 in', holeDiaExpansion_spec='0.03 in', horizontalScale=1.1982, s_npt_f_height_spec='0.5 in', s_npt_f_nominalID='`3/4', s_npt_f_wall_spec='0.08 in', verticalScale=1.261,  ):
-        self.b_npt_f_height_spec = b_npt_f_height_spec
+    def __init__(self, instanceName, doc, objPrefix="", useLabel=True, importer=None, b_npt_f_height='0.5 in', b_npt_f_nominalID='`2', b_npt_f_wall='0.08 in', base_plate_thick='0.13 in', holeDiaExpansion='0.03 in', s_npt_f_height='0.5 in', s_npt_f_nominalID='`3/4', s_npt_f_wall='0.08 in',  ):
+        self.b_npt_f_height = b_npt_f_height
         self.b_npt_f_nominalID = b_npt_f_nominalID
-        self.b_npt_f_wall_spec = b_npt_f_wall_spec
-        self.base_plate_thick_spec = base_plate_thick_spec
-        self.holeDiaExpansion_spec = holeDiaExpansion_spec
-        self.horizontalScale = horizontalScale
-        self.s_npt_f_height_spec = s_npt_f_height_spec
+        self.b_npt_f_wall = b_npt_f_wall
+        self.base_plate_thick = base_plate_thick
+        self.holeDiaExpansion = holeDiaExpansion
+        self.s_npt_f_height = s_npt_f_height
         self.s_npt_f_nominalID = s_npt_f_nominalID
-        self.s_npt_f_wall_spec = s_npt_f_wall_spec
-        self.verticalScale = verticalScale
+        self.s_npt_f_wall = s_npt_f_wall
         
         super().__init__(instanceName, doc, objPrefix=objPrefix, useLabel=useLabel, importer=importer)
         
         # import classes and create instances for directly imported objects
         from parts.npt_f import npt_f
-        b_npt_f_instance = npt_f('b_npt_f_instance', doc, objPrefix=self.objPrefix + 'b_npt_f_', useLabel=True, importer=self, femaleOD_wall_spec='0.08 in', female_height_spec='0.5 in', holeDiaExpansion_spec='0.03 in', horizontalScale=1.1982, nominalID='`2', verticalScale=1.261, )
+        b_npt_f_instance = npt_f('b_npt_f_instance', doc, objPrefix=self.objPrefix + 'b_npt_f_', useLabel=True, importer=self, diaExpansion='0.03 in', femaleOD_wall='0.08 in', female_height='0.5 in', nominalID='`2', )
         self.b_npt_f_instance = b_npt_f_instance # expose as instance variable
         self.update_imports(b_npt_f_instance) # update import info for the instance
-        b_npt_f_instance.body.Placement = Placement(Vector(0.0000, -0.0000, 20.1785), Rotation(1.0000, 0.0000, 0.0000, 0.0000))  # adjust imported object
+        b_npt_f_instance.body.Placement = Placement(Vector(0.0000, -0.0000, 16.0020), Rotation(1.0000, 0.0000, 0.0000, 0.0000))  # adjust imported object
         from parts.npt_f import npt_f
-        s_npt_f_instance = npt_f('s_npt_f_instance', doc, objPrefix=self.objPrefix + 's_npt_f_', useLabel=True, importer=self, femaleOD_wall_spec='0.08 in', female_height_spec='0.5 in', holeDiaExpansion_spec='0.03 in', nominalID='`3/4', verticalScale=1.261, )
+        s_npt_f_instance = npt_f('s_npt_f_instance', doc, objPrefix=self.objPrefix + 's_npt_f_', useLabel=True, importer=self, diaExpansion='0.03 in', femaleOD_wall='0.08 in', female_height='0.5 in', nominalID='`3/4', )
         self.s_npt_f_instance = s_npt_f_instance # expose as instance variable
         self.update_imports(s_npt_f_instance) # update import info for the instance
         
@@ -67,65 +65,41 @@ class npt_fxf(baseClass):
         self.callsheet = callsheet
         self.post_new_obj(callsheet)
         callsheet.set('A1', 'variableName')
-        callsheet.set('A10', 'base_plate_thick_spec')
-        callsheet.set('A11', 'base_plate_thick')
-        callsheet.set('A12', 'horizontalScale')
-        callsheet.set('A13', 'verticalScale')
-        callsheet.set('A14', 'holeDiaExpansion_spec')
         callsheet.set('A2', 's_npt_f_nominalID')
         callsheet.set('A3', 'b_npt_f_nominalID')
-        callsheet.set('A4', 's_npt_f_wall_spec')
-        callsheet.set('A5', 'b_npt_f_wall_spec')
-        callsheet.set('A6', 's_npt_f_height_spec')
-        callsheet.set('A7', 'b_npt_f_height_spec')
-        callsheet.set('A8', 's_npt_f_height')
-        callsheet.set('A9', 'b_npt_f_height')
+        callsheet.set('A4', 's_npt_f_wall')
+        callsheet.set('A5', 'b_npt_f_wall')
+        callsheet.set('A6', 's_npt_f_height')
+        callsheet.set('A7', 'b_npt_f_height')
+        callsheet.set('A8', 'base_plate_thick')
+        callsheet.set('A9', 'holeDiaExpansion')
         callsheet.set('B1', 'value')
-        callsheet.set('B10', '=0.13 in')
-        callsheet.setAlias('B10', 'base_plate_thick_spec')
-        callsheet.set('B11', '=0.16393 in')
-        callsheet.setAlias('B11', 'base_plate_thick')
-        callsheet.set('B12', '1.1982')
-        callsheet.setAlias('B12', 'horizontalScale')
-        callsheet.set('B13', '1.261')
-        callsheet.setAlias('B13', 'verticalScale')
-        callsheet.set('B14', '=0.03 in')
-        callsheet.setAlias('B14', 'holeDiaExpansion_spec')
         callsheet.set('B2', '`3/4')
         callsheet.setAlias('B2', 's_npt_f_nominalID')
         callsheet.set('B3', '`2')
         callsheet.setAlias('B3', 'b_npt_f_nominalID')
         callsheet.set('B4', '=0.08 in')
-        callsheet.setAlias('B4', 's_npt_f_wall_spec')
+        callsheet.setAlias('B4', 's_npt_f_wall')
         callsheet.set('B5', '=0.08 in')
-        callsheet.setAlias('B5', 'b_npt_f_wall_spec')
+        callsheet.setAlias('B5', 'b_npt_f_wall')
         callsheet.set('B6', '=0.5 in')
-        callsheet.setAlias('B6', 's_npt_f_height_spec')
+        callsheet.setAlias('B6', 's_npt_f_height')
         callsheet.set('B7', '=0.5 in')
-        callsheet.setAlias('B7', 'b_npt_f_height_spec')
-        callsheet.set('B8', '=0.6305 in')
-        callsheet.setAlias('B8', 's_npt_f_height')
-        callsheet.set('B9', '=0.6305 in')
-        callsheet.setAlias('B9', 'b_npt_f_height')
+        callsheet.setAlias('B7', 'b_npt_f_height')
+        callsheet.set('B8', '=0.13 in')
+        callsheet.setAlias('B8', 'base_plate_thick')
+        callsheet.set('B9', '=0.03 in')
+        callsheet.setAlias('B9', 'holeDiaExpansion')
         callsheet.set('C1', 'isCallParam')
-        callsheet.set('C10', 'Y')
-        callsheet.set('C11', 'N')
-        callsheet.set('C12', 'Y')
-        callsheet.set('C13', 'Y')
-        callsheet.set('C14', 'Y')
         callsheet.set('C2', 'Y')
         callsheet.set('C3', 'Y')
         callsheet.set('C4', 'Y')
         callsheet.set('C5', 'Y')
         callsheet.set('C6', 'Y')
         callsheet.set('C7', 'Y')
-        callsheet.set('C8', 'N')
-        callsheet.set('C9', 'N')
+        callsheet.set('C8', 'Y')
+        callsheet.set('C9', 'Y')
         callsheet.set('D1', 'comment')
-        callsheet.set('D11', 'base_plate_thick_spec * verticalScale')
-        callsheet.set('D12', 'BASF 17-4 xy scale 1.1982')
-        callsheet.set('D13', 'BASF 17-4 z scale 1.2610')
-        callsheet.set('D8', 's_npt_f_height_spec * verticalScale')
         callsheet.recompute()  # recompute after adding object
         
         ketch = doc.addObject('Sketcher::SketchObject', self.addPrefix('ketch') )
@@ -133,12 +107,12 @@ class npt_fxf(baseClass):
         self.ketch = ketch
         self.post_new_obj(ketch)
         self.container_append_object(base_plate, ketch)
-        geo0 = ketch.addGeometry(Part.Circle(Vector(0.0000, 0.0000, 0.0000), Vector (0.0, 0.0, 1.0), 39.0320))
-        geo1 = ketch.addGeometry(Part.Circle(Vector(0.0000, 0.0000, 0.0000), Vector (0.0, 0.0, 1.0), 18.8693))
+        geo0 = ketch.addGeometry(Part.Circle(Vector(0.0000, 0.0000, 0.0000), Vector (0.0, 0.0, 1.0), 32.5755))
+        geo1 = ketch.addGeometry(Part.Circle(Vector(0.0000, 0.0000, 0.0000), Vector (0.0, 0.0, 1.0), 15.7480))
         ketch.addConstraint(Sketcher.Constraint('Coincident', geo0, 3, -1, 1))
         ketch.addConstraint(Sketcher.Constraint('Coincident', geo1, 3, geo0, 3))
-        ketch.addConstraint(Sketcher.Constraint('Diameter', geo0, 78.0639))
-        ketch.addConstraint(Sketcher.Constraint('Diameter', geo1, 37.7385))
+        ketch.addConstraint(Sketcher.Constraint('Diameter', geo0, 65.1510))
+        ketch.addConstraint(Sketcher.Constraint('Diameter', geo1, 31.4960))
         ketch.AttacherEngine = 'Engine Plane'
         ketch.AttachmentSupport = (base_plate_XY_Plane, (''))
         base_plate_XY_Plane.Visibility = False  # hide base object
@@ -152,7 +126,7 @@ class npt_fxf(baseClass):
         self.pad = pad
         self.post_new_obj(pad)
         self.container_append_object(base_plate, pad)
-        pad.Length = 4.163822
+        pad.Length = 3.302
         pad.Profile = (ketch, [''])
         pad.ReferenceAxis = (ketch, ['N_Axis'])
         pad.Visibility = False
@@ -172,22 +146,16 @@ class npt_fxf(baseClass):
         # add delayed static property values
         
         # add expressions to object properties based on expression dependencies
-        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('holeDiaExpansion_spec'), f"=<<{self.addPrefix('callsheet')}>>.holeDiaExpansion_spec")
-        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('nominalID'), f"=<<{self.addPrefix('callsheet')}>>.s_npt_f_nominalID")
-        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('verticalScale'), f"=<<{self.addPrefix('callsheet')}>>.verticalScale")
-        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('femaleOD_wall_spec'), f"=<<{self.addPrefix('callsheet')}>>.s_npt_f_wall_spec")
-        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('female_height_spec'), f"=<<{self.addPrefix('callsheet')}>>.s_npt_f_height_spec")
-        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('holeDiaExpansion_spec'), f"=<<{self.addPrefix('callsheet')}>>.holeDiaExpansion_spec")
-        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('nominalID'), f"=<<{self.addPrefix('callsheet')}>>.b_npt_f_nominalID")
-        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('horizontalScale'), f"=<<{self.addPrefix('callsheet')}>>.horizontalScale")
-        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('verticalScale'), f"=<<{self.addPrefix('callsheet')}>>.verticalScale")
-        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('femaleOD_wall_spec'), f"=<<{self.addPrefix('callsheet')}>>.b_npt_f_wall_spec")
-        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('female_height_spec'), f"=<<{self.addPrefix('callsheet')}>>.b_npt_f_height_spec")
-        callsheet.set(callsheet.getCellFromAlias("base_plate_thick"), f"=base_plate_thick_spec * verticalScale")
-        callsheet.set(callsheet.getCellFromAlias("s_npt_f_height"), f"=s_npt_f_height_spec * verticalScale")
-        callsheet.set(callsheet.getCellFromAlias("b_npt_f_height"), f"=b_npt_f_height_spec * verticalScale")
         pad.setExpression("Length", f"<<{self.addPrefix('callsheet')}>>.base_plate_thick")
+        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('nominalID'), f"=<<{self.addPrefix('callsheet')}>>.s_npt_f_nominalID")
+        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('femaleOD_wall'), f"=<<{self.addPrefix('callsheet')}>>.s_npt_f_wall")
+        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('female_height'), f"=<<{self.addPrefix('callsheet')}>>.s_npt_f_height")
+        s_npt_f_instance.callsheet.set(s_npt_f_instance.callsheet.getCellFromAlias('diaExpansion'), f"=<<{self.addPrefix('callsheet')}>>.holeDiaExpansion")
         b_npt_f_instance.body.setExpression('.Placement.Base.z', f"<<{self.addPrefix('callsheet')}>>.base_plate_thick + <<{self.addPrefix('callsheet')}>>.b_npt_f_height")
+        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('nominalID'), f"=<<{self.addPrefix('callsheet')}>>.b_npt_f_nominalID")
+        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('femaleOD_wall'), f"=<<{self.addPrefix('callsheet')}>>.b_npt_f_wall")
+        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('female_height'), f"=<<{self.addPrefix('callsheet')}>>.b_npt_f_height")
+        b_npt_f_instance.callsheet.set(b_npt_f_instance.callsheet.getCellFromAlias('diaExpansion'), f"=<<{self.addPrefix('callsheet')}>>.holeDiaExpansion")
         ketch.setExpression("Constraints[2]", f"<<{self.addPrefix('b_npt_f_callsheet')}>>.femaleOD")
         ketch.setExpression("Constraints[3]", f"<<{self.addPrefix('s_npt_f_callsheet')}>>.femaleOD")
         
@@ -198,16 +166,14 @@ class npt_fxf(baseClass):
         # now we have rebuilt the original npt_fxf doc. Now we apply dynmic call parameters
         print("there can be temporary errors when we applying dynamic call parameters that change original npt_fxf's shape.")
         print("ignore temporary errors, if any, below.")
-        callsheet.set(callsheet.getCellFromAlias('base_plate_thick_spec'), f'={self.base_plate_thick_spec}')
-        callsheet.set(callsheet.getCellFromAlias('horizontalScale'), f'{self.horizontalScale}')
-        callsheet.set(callsheet.getCellFromAlias('verticalScale'), f'{self.verticalScale}')
-        callsheet.set(callsheet.getCellFromAlias('holeDiaExpansion_spec'), f'={self.holeDiaExpansion_spec}')
         callsheet.set(callsheet.getCellFromAlias('s_npt_f_nominalID'), f'{self.s_npt_f_nominalID}')
         callsheet.set(callsheet.getCellFromAlias('b_npt_f_nominalID'), f'{self.b_npt_f_nominalID}')
-        callsheet.set(callsheet.getCellFromAlias('s_npt_f_wall_spec'), f'={self.s_npt_f_wall_spec}')
-        callsheet.set(callsheet.getCellFromAlias('b_npt_f_wall_spec'), f'={self.b_npt_f_wall_spec}')
-        callsheet.set(callsheet.getCellFromAlias('s_npt_f_height_spec'), f'={self.s_npt_f_height_spec}')
-        callsheet.set(callsheet.getCellFromAlias('b_npt_f_height_spec'), f'={self.b_npt_f_height_spec}')
+        callsheet.set(callsheet.getCellFromAlias('s_npt_f_wall'), f'={self.s_npt_f_wall}')
+        callsheet.set(callsheet.getCellFromAlias('b_npt_f_wall'), f'={self.b_npt_f_wall}')
+        callsheet.set(callsheet.getCellFromAlias('s_npt_f_height'), f'={self.s_npt_f_height}')
+        callsheet.set(callsheet.getCellFromAlias('b_npt_f_height'), f'={self.b_npt_f_height}')
+        callsheet.set(callsheet.getCellFromAlias('base_plate_thick'), f'={self.base_plate_thick}')
+        callsheet.set(callsheet.getCellFromAlias('holeDiaExpansion'), f'={self.holeDiaExpansion}')
         doc.recompute()
         update_doc_seName(doc, refreshCache=True) # call params may change shape, so we update face/edge names.
         print("ignore temporary errors, if any, above.")
@@ -221,7 +187,7 @@ def main():
     doc = recreate_tmp_doc()
     
     # create instance of npt_fxf
-    myInstance = npt_fxf("myInstance", doc, objPrefix="", useLabel=True, importer=None, b_npt_f_height_spec='0.5 in', b_npt_f_nominalID='`2', b_npt_f_wall_spec='0.08 in', base_plate_thick_spec='0.13 in', holeDiaExpansion_spec='0.03 in', horizontalScale=1.1982, s_npt_f_height_spec='0.5 in', s_npt_f_nominalID='`3/4', s_npt_f_wall_spec='0.08 in', verticalScale=1.261, )
+    myInstance = npt_fxf("myInstance", doc, objPrefix="", useLabel=True, importer=None, b_npt_f_height='0.5 in', b_npt_f_nominalID='`2', b_npt_f_wall='0.08 in', base_plate_thick='0.13 in', holeDiaExpansion='0.03 in', s_npt_f_height='0.5 in', s_npt_f_nominalID='`3/4', s_npt_f_wall='0.08 in', )
     
     # main_part2
     from pprint import pformat
